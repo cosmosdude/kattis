c69a036c95929a75171d646c712a264e4ec4772f
Thwin Htoo Aung <thwinhtooaung@better.hr>
2021-10-18 02:51:35 +0630

    nsum problem

---
+++ nsum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    int total = 0;
+    while(n--) {
+        int a; cin >> a;
+        total += a;
+    }
+    cout << total << endl;
+    return 0;
+}
