40ca5617a6d7a2f9d82762a108ed64d7f2b278e2
Thwin Htoo Aung <thwinhtooaungreborn@gmail.com>
2022-02-12 02:43:51 +0630

    math geometry constructive-algo

---
+++ src/halfacookie.cpp
@@ -0,0 +1,93 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+
+// long long
+typedef long long Long;
+typedef long double BFloat;
+// vector strings
+typedef vector<string> Vstring;
+// vector primitives
+typedef vector<char> Vchar; typedef vector<Vchar> VVchar;
+typedef vector<int> Vint; typedef vector<Vint> VVint;
+typedef vector<Long> Vlong; typedef vector<Vint> VVlong;
+
+// declare var and immediately cin
+#define cinvar(type, name)\
+	type name; cin >> name;
+
+#define cinchar(name) cinvar(char, name)
+#define cinuint(name) cinvar(unsigned int, name)
+#define cinint(name) cinvar(int, name)
+#define cinlong(var) cinvar(long long, var)
+#define cinulong(var) cinvar(unsigned long long, var)
+#define cindouble(var) cinvar(double, var)
+#define cinstring(var) cinvar(string, var)
+// declare var and immediately getline
+#define cinline(var)\
+	string var; getline(cin, var);
+
+#define cinvector(type, name, n) \
+	vector<type> name(n);\
+	for(auto& e: name) cin >> e;
+
+
+#define twhile(a)\
+	int a; cin >> a; cin.ignore(); while(a--)
+#define tfor(i, a)\
+	int a; cin >> a; cin.ignore(); for(int i = 0; i < a; i++)
+
+// quick for
+#define f(i, a, n) for(int i = a; i < n; i++)
+#define fe(i, a, n) for(int i = a; i <= n; i++)
+#define fr(i, a, n) for(int i = n-1; i> a; i--)
+#define fer(i, a, n) for(int i = n-1; i >= a; i--)
+
+#define fstr(i, s) f(i, 0, s.length())
+#define ferstr(i, s) fr(i, s.length()-1, 0)
+
+#define pvc(target, v, c)\
+	for(int i = 0; i < v.size(); i++)\
+		target << v[i] << (i==v.size()-1 ? '\n': c);
+
+#define pv(v)\
+	pvc(cout, v, ' ')
+
+//----------------------
+bool isvalid(double r, double x, double y) {
+	return sqrt(x*x + y*y) <= r;
+}
+
+double circlearea(double r, double angle = M_PI) {
+	return angle * r * r;
+}
+
+double smallarea(double r, double x, double y) {
+	double a = sqrt(x*x + y*y);
+
+	double angle = acos(a/r);
+
+	double arcarea = circlearea(r, angle);
+
+	double b = sin(angle) * r;
+
+	double triAreaTotal = a*b;
+
+	return arcarea - triAreaTotal;
+}
+
+int main() {
+
+	cindouble(r); cindouble(x); cindouble(y);
+
+	while(!cin.fail()) {
+		if (isvalid(r, x, y)) {
+			double sarea = smallarea(r, x, y);
+			cout << circlearea(r) - sarea << " " << sarea << endl;
+		} else cout << "miss" << endl;
+		cin >> r >> x >> y;
+	}
+	
+	return 0;
+}
+
