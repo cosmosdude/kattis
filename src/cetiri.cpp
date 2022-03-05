02f243a94ce91e4cb9a1a2efc693bc54918db971
Thwin Htoo Aung <thwinhtooaungreborn@gmail.com>
2022-03-05 17:33:10 +0630

    Cetiri

---
+++ src/cetiri.cpp
@@ -0,0 +1,76 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+
+// long long
+typedef long long Long;
+typedef long double BFloat;
+// vector strings
+typedef vector<string> Vstring;
+// vector primitives
+typedef vector<char> Vchar; typedef vector<Vchar> VVchar;
+typedef vector<int> Vint; typedef vector<Vint> VVint;
+typedef vector<Long> Vlong; typedef vector<Vint> VVlong;
+
+// declare var and immediately cin
+#define cinvar(type, name)\
+	type name; cin >> name;
+
+#define cinchar(name) cinvar(char, name)
+#define cinuint(name) cinvar(unsigned int, name)
+#define cinint(name) cinvar(int, name)
+#define cinlong(var) cinvar(long long, var)
+#define cinulong(var) cinvar(unsigned long long, var)
+#define cindouble(var) cinvar(double, var)
+#define cinstring(var) cinvar(string, var)
+// declare var and immediately getline
+#define cinline(var)\
+	string var; getline(cin, var);
+
+#define cinvector(type, name, n) \
+	vector<type> name(n);\
+	for(auto& e: name) cin >> e;
+
+
+#define twhile(a)\
+	int a; cin >> a; cin.ignore(); while(a--)
+#define tfor(i, a)\
+	int a; cin >> a; cin.ignore(); for(int i = 0; i < a; i++)
+
+// quick for
+#define f(i, a, n) for(int i = a; i < n; i++)
+#define fe(i, a, n) for(int i = a; i <= n; i++)
+#define fr(i, a, n) for(int i = n-1; i> a; i--)
+#define fer(i, a, n) for(int i = n-1; i >= a; i--)
+
+#define fstr(i, s) f(i, 0, s.length())
+#define ferstr(i, s) fr(i, s.length()-1, 0)
+
+#define pvc(target, v, c)\
+	for(int i = 0; i < v.size(); i++)\
+		target << v[i] << (i==v.size()-1 ? '\n': c);
+
+#define pv(v)\
+	pvc(cout, v, ' ')
+
+//----------------------
+
+int main() {
+
+	vector<int> a(3);
+
+	cin >> a[0] >> a[1] >> a[2];
+
+	sort(a.begin(), a.end());
+
+	int d1 = a[1]-a[0];
+	int d2 = a[2]-a[1];
+
+	int diff = max(d1, d2) / 2;
+	if (d1 > d2) cout << a[0]+diff << endl;
+	else if (d2 > d1) cout << a[1]+diff << endl;
+	else cout << a[2]+d1 << endl;
+	
+	return 0;
+}
+
